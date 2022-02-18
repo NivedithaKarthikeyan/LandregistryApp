bf3a0ab79a1c7f65b8da09b40838c4c7688315a2
Grand Master Coder <grand.master.coder@gmail.com>
2022-02-18 16:23:12 +0000

    Update docs/source/conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -51,7 +51,7 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-html_context = {
-    'css_files': ['css/extra.css'],
-}
+# html_context = {
+#    'css_files': ['css/extra.css'],
+# }
 
